bcbd568c6591d28d8fb0f39dc193181da48b774e
Sabarish <sabarish1182002@gmail.com>
2025-07-31 14:50:43 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import NoticeBoard from './components/NoticeBoard';
 import NoticeDetails from './components/NoticeDetails';
 
 function App() {
   return (
+    <HashRouter>
     <Routes>
       <Route path="/" element={<NoticeBoard />} />
       <Route path="/notice/:id" element={<NoticeDetails />} />
     </Routes>
+    </ HashRouter>
+
   );
 }
 
